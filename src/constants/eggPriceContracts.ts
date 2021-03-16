f17a5254a3e9e88dfdd3e12e7166501800ec621a
freezerfarmfinance <freezerfarmfinance@protonmail.com>
2021-03-16 17:24:56 -0600

    100%

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0xf952fc3ca7325cc27d15885d37117676d25bfda6',
+  cakeAddress: '0x35d81fe74D0CFC375Deba9aD093736C536794dF7',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019'
+  lpAddress: '0xa50093755F66B8BB4422821B51630B76C67F9540'
 }
 
 export default priceContracts
