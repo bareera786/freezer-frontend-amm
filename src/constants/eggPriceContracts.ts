789b22fb8ff5908cb244b3c45a0f258e0608cfb7
bareera786 <rplshop@gmail.com>
2021-04-07 06:11:30 +0500

    Update eggPriceContracts.ts

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,5 +1,5 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0x35d81fe74D0CFC375Deba9aD093736C536794dF7',
+  cakeAddress: '0x9Ef97bf5011A67494f32f55F54Ee8f5695ac6cd0',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
   lpAddress: '0xa50093755F66B8BB4422821B51630B76C67F9540'
 }
